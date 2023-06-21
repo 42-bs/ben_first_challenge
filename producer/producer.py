c8b8ae0e70a5f1eae00db2bf9825765b9307459f
rodrigo-br <rodrigoab123@gmail.com>
2023-06-21 13:26:52 -0300

    consumer dotnet project with custom deserializer

---
+++ producer/producer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaProducer
+import json
+import randomDataGenerator
+import time
+
+TOPIC_NAME = 'random_energy_data'
+KAFKA_SERVER = 'localhost:9092'
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, key_serializer=str.encode, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+while(True):
+    id, infos = randomDataGenerator.generate_random_data()
+
+    compania = json.dumps(infos)
+    producer.send(TOPIC_NAME, key=str(id), value=compania)
+    time.sleep(1)
+
+
