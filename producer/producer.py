aea1c53aeb145e5dccff961f4e2eae875bb41497
rodrigo-br <rodrigoab123@gmail.com>
2023-06-21 16:24:40 -0300

    producer and kafka Dockerfile

--- producer/producer.py
+++ producer/producer.py
@@ -6,13 +6,17 @@ import time
 TOPIC_NAME = 'random_energy_data'
 KAFKA_SERVER = 'localhost:9092'
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, key_serializer=str.encode, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+#producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, key_serializer=str.encode, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+start = time.time()
+
 
 while(True):
     id, infos = randomDataGenerator.generate_random_data()
 
     compania = json.dumps(infos)
-    producer.send(TOPIC_NAME, key=str(id), value=compania)
+    #producer.send(TOPIC_NAME, key=str(id), value=compania)
     time.sleep(1)
+    print(infos)
 
 
