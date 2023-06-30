a8168b664418a8144e1f7abe5d379a026e687efe
Robson Inoue <robson.inoue@gmail.com>
2023-06-30 18:01:05 -0300

    created solution for .net projects and added stylecop rules into it.

---
+++ consumer/EnergyData.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="EnergyData.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Consumer
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// This class get data received from Kafka and convert to Database schema.
+    /// </summary>
+    public class EnergyData
+    {
+        /// <summary>
+        /// Gets or Sets Get any text here.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Get any text here.
+        /// </summary>
+        public long CompanyId { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Get any text here.
+        /// </summary>
+        public string? ConsumerUnity { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Get any text here.
+        /// </summary>
+        public double? Value { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Get any text here.
+        /// </summary>
+        [Column(TypeName = "DATE")]
+        public DateTime Timestamp { get; set; }
+    }
+}
