8137df902b5905b875d13859d24e18a8689e3eab
rodrigo-br <rodrigoab123@gmail.com>
2023-07-04 10:08:28 -0300

    documenting producer

--- producer/srcs/energy_data.py
+++ producer/srcs/energy_data.py
@@ -1,11 +1,32 @@
 class EnergyData:
+	"""
+	Object to be serialized and sent as a message to kafka.
+	This class needs to be built with a builder, which sets up
+	all the rules and restrictions about the information.
+	"""
+
 	def __init__(self, builder):
+		"""
+		Constructs the EnergyData object based on passed builder.
+
+		Args:
+			builder EnergyDataBuilder: This class maintain the configuration, rules
+			and restrictions about every variable of the EnergyData object.
+		"""
+
 		self._id = builder.id()
 		self._consumer_unity = builder.consumer_unity()
 		self._value = builder.value()
 		self._date = builder.date()
 
 	def to_dict(self):
+		"""
+		Convert the current object to a dictionary
+
+		Returns:
+			Dictionary: a dictionary representation of class with variable name as key and the variable value as value.
+		"""
+
 		return { "id": self._id,
             "consumer_unity": self._consumer_unity,
             "value": self._value,
--- producer/srcs/energy_data_kafka_producer.py
+++ producer/srcs/energy_data_kafka_producer.py
@@ -5,10 +5,27 @@ import json
 import time
 
 class EnergyDataKafkaProducer:
+	"""
+	A class to represent the producer. It only produces EnergyData as msg
+	serialized as dict and uses a builder to specify how this EnergyData will be built.
+	"""
+
 	def __init__(self,
 		server='kafka:9092',
 		topic='random_energy_data',
 		retry_limit=30):
+		"""
+		Constructs all the necessary attributes to connect to a kafka server and produces
+		a msg to a topic with a EnergyData object.
+
+		Args:
+			server (str, optional): the address of the kafka server. Defaults to 'kafka:9092'.
+			topic (str, optional): the target topic for the message. Defaults to 'random_energy_data'.
+			retry_limit (int, optional): How many times it will try to reconnect if connection fail. Defaults to 30.
+
+		the producer is a Kafka client that publishes records to the Kafka cluster.
+		the builder is the class that will shape and construct the EnergyData information.
+		"""
 		self._server = server
 		self._topic = topic
 		self._retry_limit = retry_limit
@@ -16,6 +33,16 @@ class EnergyDataKafkaProducer:
 		self._builder = EnergyDataBuilder()
 
 	def connect_to_kafka(self):
+		"""
+		establishes connection to the target kafka server
+
+		Raises:
+			ConnectionError: in case of 'retry_limit' failures, this error is raised
+
+		Returns:
+			KafkaProducer: A Kafka client that publishes records to the Kafka cluster. 
+		"""
+
 		while (self._retry_limit > 0):
 			try:
 				producer = KafkaProducer(bootstrap_servers=self._server)
@@ -26,5 +53,12 @@ class EnergyDataKafkaProducer:
 		raise ConnectionError
 
 	def send_through_kafka(self):
+		"""
+		Serializes the message and send it through the KafkaProducer to the target topic
+
+		Returns:
+			FutureRecordMetadata: resolves to RecordMetadata
+		"""
+
 		message = json.dumps(EnergyData(self._builder).to_dict())
 		return self._producer.send(self._topic, message.encode('utf-8'))
