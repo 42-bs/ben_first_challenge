697a8d0915c32eeacfeae192ca2154a337a6fb7d
Robson Inoue <robson.inoue@gmail.com>
2023-07-10 15:56:30 -0300

    create a DB if not exists in db2

--- api2/backend_python/app/db.py
+++ api2/backend_python/app/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 import os
 
 db_user = os.getenv("DB_USER")
@@ -12,6 +13,8 @@ db_port = "1433"
 SQLALCHEMY_DATABASE_URL = f'mssql+pymssql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if not database_exists(engine.url):
+	create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
