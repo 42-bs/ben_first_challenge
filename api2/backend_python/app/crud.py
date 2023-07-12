6f62f8854644f5dccf2342ece0c88d5708be3e19
renanguillen <rigcpj@gmail.com>
2023-07-12 13:45:24 -0300

    minor changes to minimize errors, added comments

--- api2/backend_python/app/crud.py
+++ api2/backend_python/app/crud.py
@@ -3,10 +3,26 @@ from app.models import DataEnergy
 from app.energy_data_serializer import energy_data_deserializer
 
 def create_data_energy(db: Session):
-	energy_data_received = energy_data_deserializer()
-	db.add_all(energy_data_received)
-	db.commit()
-	return ("Data got from api1. Go for root to see all entries")
+	"""
+	Creates energy data in the database from the external API.
+
+	Returns:
+	The success message as a string.
+	"""
+	try:
+		energy_data_received = energy_data_deserializer()
+		db.add_all(energy_data_received)
+		db.commit()
+		return ("Data got from api1. Go for root to see all entries")
+	except Exception as e:
+		db.rollback()
+		raise e
 
 def get_all_energy_data(db: Session):
+	"""
+	Retrieves all energy data entries from the database.
+
+	Returns:
+	A list of all energy data entries.
+	"""
 	return db.query(DataEnergy).all()
--- api2/backend_python/app/schemas.py
+++ api2/backend_python/app/schemas.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel
 
 class EnergyDataCreate(BaseModel):
-    id: int
-    company_id: int
-    consumer_unity: str
-    value: float
-    timestamp: float
+	"""
+	The EnergyDataCreate Pydantic model. Represents the structure of energy data for creation.
 
-    class Config:
-        orm_mode = True
+	Attributes:
+	id (int): The ID of the energy data entry.
+	company_id (int): The ID of the company.
+	consumer_unity (str): The consumer unit.
+	value (float): The energy value.
+	timestamp (float): The timestamp.
+	"""
+	id: int
+	company_id: int
+	consumer_unity: str
+	value: float
+	timestamp: float
+
+	class Config:
+		orm_mode = True
