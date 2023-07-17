0feb6cd0f38e66b12f7a0e32c3bf5ce3894c356f
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-07-17 15:57:59 -0300

    containerising the front-end

--- api2/backend_python/app/db.py
+++ api2/backend_python/app/db.py
@@ -13,6 +13,7 @@ db_port = os.getenv("DB_PORT", "1433")
 SQLALCHEMY_DATABASE_URL = f'mssql+pymssql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 if not database_exists(engine.url):
 	create_database(engine.url)
 
